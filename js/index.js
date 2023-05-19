992e6f67f93a063214f85e47dfeff09e4f19db91
kingofvibe <vibecity112@gmail.com>
2023-05-19 23:09:33 +0100

    off to responsiveness

--- js/index.js
+++ js/index.js
@@ -13,10 +13,34 @@ document.body.onpointermove = e => {
  
 }
 
-// animate expand to follow cursor
-w_box.forEach( e => {
-    console.log(e);
-    e.addEventListener( "click", e => {
-        console.log({e});
-     })
-})
+// emoji animation
+
+document.querySelector('body').addEventListener('mousemove', rotateEye)
+
+function rotateEye  (){
+   let eyes = document.querySelectorAll('.eye')
+   eyes.forEach(eye => {
+      let x = eye.getBoundingClientRect().left + eye.clientWidth / 2
+      let y = eye.getBoundingClientRect().top + eye.clientHeight / 2
+      let radian = Math.atan2(event.pageX - x, event.pageY - y)
+      let rotate = radian  * (180 / Math.PI) * -1 + 270
+      console.log(rotate);
+      eye.style.transform = `rotate(${rotate}deg)`
+   })
+}
+
+
+// show image marque 
+
+// work boxes
+const w_box_1 = document.getElementById('w-box-1')
+const w_box_2 = document.getElementById('w-box-2')
+const w_box_3 = document.getElementById('w-box-3')
+const w_box_4 = document.getElementById('w-box-4')
+
+// work image containers
+const pf_1 = document.getElementById('p-f-1')
+const pf_2 = document.getElementById('p-f-2')
+const pf_3 = document.getElementById('p-f-3')
+const pf_4 = document.getElementById('p-f-4')
+
